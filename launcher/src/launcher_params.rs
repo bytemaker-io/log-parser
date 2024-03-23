c6b9c7d50602b8e0a322eff850f5101676b7e9fa
kaelan <git@bytemaker.io>
2024-03-23 17:32:39 +0100

    update release cicd

--- launcher/src/launcher_params.rs
+++ launcher/src/launcher_params.rs
@@ -6,6 +6,7 @@ use clap::{arg, Parser};
 #[command(about = "This program is used to load the log file and parse it to frame the data. It will also upload the data to ElasticSearch.")]
 #[command(author="Fei wang")]
 #[command(version)]
+#[command(name = "launcher")]
 pub struct Args {
     /// Name of the file,you can provide multiple file names, each file name should be separated by a space. Example: -f file1.log file2.log file3.log
     #[arg(short='f', long,required = true, num_args = 1..)]
